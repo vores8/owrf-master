cfec62f6094f47d698497b642b87eda1d64ccee0
vores8 <dbkrasn@gmail.com>
2014-02-14 19:27:37 +0400

    versio 0.9 beta

--- main.c
+++ main.c
@@ -17,18 +17,16 @@
 
 #include <nrf24l01.h>
 
-#include "debugprint.h"
+#include <debugprint.h>
+#include <owrfreadwrite.h>
 
-static uint8_t transfer_buf[64];
+static uint8_t transfer_buf[255];
 
 int bytesRead = 0;
 uint8_t channel = -1;
-uint8_t* msg;
 int phase = 0;
 
 int main(void) {
-
-	msg = transfer_buf;
 
 	uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(57600, F_CPU));
 
@@ -40,7 +38,7 @@ int main(void) {
 	nrf24l01_setrxaddr(3, (uint8_t*) "OWRF3");
 	nrf24l01_setrxaddr(4, (uint8_t*) "OWRF4");
 	nrf24l01_setrxaddr(5, (uint8_t*) "OWRF5");
-	nrf24l01_enabledynamicpayloads();
+//	nrf24l01_enabledynamicpayloads();
 
 	sei();
 
@@ -59,12 +57,11 @@ int main(void) {
 			}
 			if (bytesRead == 4) {
 				channel = transfer_buf[1];
-				msg = transfer_buf + 2;
 				debugPrint(
 						"received command channel=%02X cmd=%02X len=%02X\r\n",
-						channel, msg[0], msg[1]);
+						channel, transfer_buf[2], transfer_buf[3]);
 				bytesRead = 0;
-				if (msg[1] == 0) {
+				if (transfer_buf[3] == 0) {
 					phase = 4;
 				} else {
 					phase = 3;
@@ -73,12 +70,12 @@ int main(void) {
 		}
 			break;
 		case 3: {
-			if (bytesRead == msg[1]) {
+			if (bytesRead == transfer_buf[3]) {
 				phase = 4;
 			} else {
 				if (uart_available() > 0) {
 					databyte = uart_getc();
-					*(msg + bytesRead + 2) = databyte;
+					*(transfer_buf + bytesRead + 4) = databyte;
 					bytesRead++;
 				}
 			}
@@ -86,61 +83,59 @@ int main(void) {
 			break;
 		case 4: {
 			if (channel >= 0) {
-				uint8_t tosend[32];
-				uint8_t len = msg[1] + 2;
-				memcpy(tosend, msg, len);
-				for (int i = 0; i < len; i++) {
-					debugPrint(">%02X", tosend[i]);
-				}
-				debugPrint("\r\n");
+				uint8_t len = transfer_buf[3] + 3;
 				char* addr = "OWRF1";
 				addr[4] += channel;
-				debugPrint("send to %s %d:", addr, len);
-				nrf24l01_settxaddr((uint8_t*)addr);
-//				nrf24l01_setrxaddr(0, (uint8_t*)addr);
-				uint8_t writeret = nrf24l01_write(tosend, len);
-//				nrf24l01_setrxaddr(0, (uint8_t*) "OWRF0");
-//				nrf24l01_setrxaddr(2, (uint8_t*) "OWRF0");
-				if (writeret == 1) {
+
+				debugPrint("send to %s %d\r\n", addr, len);
+				for (int i = 0; i < len; i++) {
+					debugPrint(">%02X", transfer_buf[i + 1]);
+				}
+				debugPrint("\r\n");
+
+//				nrf24l01_settxaddr((uint8_t*)"OWRF0");
+				if (owrf_write(transfer_buf + 1, len ) == 1) {
 					debugPrint("OK\r\n");
 					phase = 5;
 				} else {
 					debugPrint("FAIL\r\n");
-					phase = 0;
-					bytesRead = 0;
-					msg = transfer_buf;
+					phase = 6;
 				}
 			}
 		}
 			break;
 		case 5: {
-//			phase = 0;
-//			bytesRead = 0;
-//			msg = transfer_buf;
-//			nrf24l01_settxaddr((uint8_t*) "OWRF0");
-			uint8_t pipe = 0xff;
-			uint8_t timeout = 10;
-			while (!nrf24l01_readready(&pipe) && (timeout > 0)) {
-				_delay_ms(5);
-				timeout--;
-			}
-			if (timeout > 0) {
-				int len = nrf24l01_getdynamicpayloadsize();
-				nrf24l01_read(transfer_buf, len);
-//				uart_putc(pipe);
-				debugPrint("channel = %d outlen=%d\r\n", pipe, len);
-				for (int i = 0; i < len; i++) {
+//			nrf24l01_setrxaddr(0, (uint8_t*) "OWRF1");
+
+			int length = -1;
+			length = owrf_read(transfer_buf);
+//			nrf24l01_setrxaddr(0, (uint8_t*) "OWRF0");
+			if (length != -1) {
+				for (int i = 0; i < length; i++) {
 					uart_putc(transfer_buf[i]);
 					debugPrint("<%02X", transfer_buf[i]);
 				}
 				debugPrint("\r\n");
 				uart_flush();
+				phase = 7;
 			} else {
 				debugPrint("TIMEOUT\r\n");
+				phase = 6;
 			}
+		}
+			break;
+		case 6: {
+			uart_putc(channel);
+			uart_putc(0xFF);
+			uart_putc(0x00);
+			uart_flush();
 			phase = 0;
 			bytesRead = 0;
-			msg = transfer_buf;
+		}
+			break;
+		case 7: {
+			phase = 0;
+			bytesRead = 0;
 		}
 			break;
 		}
